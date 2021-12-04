b3fbb1a0f60da28e4a54c72e5c02969607219218
Shreyash <60093808+DrCybernotix@users.noreply.github.com>
2021-12-04 11:24:06 +0530

    Create convertTensors.py

---
+++ convertTensors.py
@@ -0,0 +1,6 @@
+# Turn our data into TensorFlow Datasets
+train_dataset = tf.data.Dataset.from_tensor_slices((train_sentences, train_labels_one_hot))
+valid_dataset = tf.data.Dataset.from_tensor_slices((val_sentences, val_labels_one_hot))
+test_dataset = tf.data.Dataset.from_tensor_slices((test_sentences, test_labels_one_hot))
+
+train_dataset
