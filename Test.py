ab2cd3898e95ee92a1a27a56acacfff60922dcd2
Shreyash <60093808+DrCybernotix@users.noreply.github.com>
2021-12-04 10:10:20 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,148 @@
+import os
+import pandas as pd
+import sklearn
+import matplotlib as plt
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+
+# Check for GPU
+!nvidia-smi -L
+
+!git clone https://github.com/Franck-Dernoncourt/pubmed-rct.git
+!ls pubmed-rct
+
+# Check what files are in the PubMed_20K dataset 
+!ls pubmed-rct/PubMed_20k_RCT_numbers_replaced_with_at_sign
+# Start by using the 20k dataset
+data_dir = "pubmed-rct/PubMed_20k_RCT_numbers_replaced_with_at_sign/"
+
+
+filenames = [data_dir + filename for filename in os.listdir(data_dir)]
+filenames
+
+# Create function to read the lines of a document
+def get_lines(filename):
+  """
+  Reads filename (a text file) and returns the lines of text as a list.
+  
+  Args:
+      filename: a string containing the target filepath to read.
+  
+  Returns:
+      A list of strings with one string per line from the target filename.
+      For example:
+      ["this is the first line of filename",
+       "this is the second line of filename",
+       "..."]
+  """
+  with open(filename, "r") as f:
+    return f.readlines()
+  
+train_lines = get_lines(data_dir+"train.txt")
+train_lines[:20] # the whole first example of an abstract + a little more of the next one
+
+def preprocess_text_with_line_numbers(filename):
+  """Returns a list of dictionaries of abstract line data.
+
+  Takes in filename, reads its contents and sorts through each line,
+  extracting things like the target label, the text of the sentence,
+  how many sentences are in the current abstract and what sentence number
+  the target line is.
+
+  Args:
+      filename: a string of the target text file to read and extract line data
+      from.
+
+  Returns:
+      A list of dictionaries each containing a line from an abstract,
+      the lines label, the lines position in the abstract and the total number
+      of lines in the abstract where the line is from. For example:
+
+      [{"target": 'CONCLUSION',
+        "text": The study couldn't have gone better, turns out people are kinder than you think",
+        "line_number": 8,
+        "total_lines": 8}]
+  """
+  input_lines = get_lines(filename) # get all lines from filename
+  abstract_lines = "" # create an empty abstract
+  abstract_samples = [] # create an empty list of abstracts
+  
+  # Loop through each line in target file
+  for line in input_lines:
+    if line.startswith("###"): # check to see if line is an ID line
+      abstract_id = line
+      abstract_lines = "" # reset abstract string
+    elif line.isspace(): # check to see if line is a new line
+      abstract_line_split = abstract_lines.splitlines() # split abstract into separate lines
+
+      # Iterate through each line in abstract and count them at the same time
+      for abstract_line_number, abstract_line in enumerate(abstract_line_split):
+        line_data = {} # create empty dict to store data from line
+        target_text_split = abstract_line.split("\t") # split target label from text
+        line_data["target"] = target_text_split[0] # get target label
+        line_data["text"] = target_text_split[1].lower() # get target text and lower it
+        line_data["line_number"] = abstract_line_number # what number line does the line appear in the abstract?
+        line_data["total_lines"] = len(abstract_line_split) - 1 # how many total lines are in the abstract? (start from 0)
+        abstract_samples.append(line_data) # add line data to abstract samples list
+    
+    else: # if the above conditions aren't fulfilled, the line contains a labelled sentence
+      abstract_lines += line
+  
+  return abstract_samples
+
+# Get data from file and preprocess it
+%%time
+train_samples = preprocess_text_with_line_numbers(data_dir + "train.txt")
+val_samples = preprocess_text_with_line_numbers(data_dir + "dev.txt") # dev is another name for validation set
+test_samples = preprocess_text_with_line_numbers(data_dir + "test.txt")
+len(train_samples), len(val_samples), len(test_samples)
+
+# Check the first abstract of our training data
+train_samples[:14]
+
+
+train_df = pd.DataFrame(train_samples)
+val_df = pd.DataFrame(val_samples)
+test_df = pd.DataFrame(test_samples)
+train_df.head(14)
+
+# Distribution of labels in training data
+train_df.target.value_counts()
+
+train_df.total_lines.plot.hist();
+
+# Convert abstract text lines into lists 
+train_sentences = train_df["text"].tolist()
+val_sentences = val_df["text"].tolist()
+test_sentences = test_df["text"].tolist()
+len(train_sentences), len(val_sentences), len(test_sentences)
+
+# View first 10 lines of training sentences
+train_sentences[:10]
+
+# One hot encode labels
+from sklearn.preprocessing import OneHotEncoder
+one_hot_encoder = OneHotEncoder(sparse=False)
+train_labels_one_hot = one_hot_encoder.fit_transform(train_df["target"].to_numpy().reshape(-1, 1))
+val_labels_one_hot = one_hot_encoder.transform(val_df["target"].to_numpy().reshape(-1, 1))
+test_labels_one_hot = one_hot_encoder.transform(test_df["target"].to_numpy().reshape(-1, 1))
+
+# Check what training labels look like
+train_labels_one_hot
+
+# Extract labels ("target" columns) and encode them into integers 
+from sklearn.preprocessing import LabelEncoder
+label_encoder = LabelEncoder()
+train_labels_encoded = label_encoder.fit_transform(train_df["target"].to_numpy())
+val_labels_encoded = label_encoder.transform(val_df["target"].to_numpy())
+test_labels_encoded = label_encoder.transform(test_df["target"].to_numpy())
+
+# Check what training labels look like
+train_labels_encoded
+
+# Get class names and number of classes from LabelEncoder instance 
+num_classes = len(label_encoder.classes_)
+class_names = label_encoder.classes_
+num_classes, class_names
